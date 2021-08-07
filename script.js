3c28e6c381f16edc27eb8dd67720da87838af46b
mleblanc94 <leblancmichael94@gmail.com>
2021-08-07 13:01:27 -0400

    added all functionality to JS

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const button = document.getElementById('button');
 
 // Prompt user to select media stream, pass that to video element, then play.
 async function selectMediaStream() {
-    try{
-
-    } catch(error) {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
         // Catch errors
-        console.log('Whoops, something went wrong:' error);
+        console.log('Whoops, something went wrong:', error);
     }
 }
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
 
 // On load
 selectMediaStream();
