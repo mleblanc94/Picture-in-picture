709dd02c342bbb2d4f17b470e270a6793d3958fe
mleblanc94 <leblancmichael94@gmail.com>
2021-08-07 12:40:49 -0400

    started Javascript portion of page

---
+++ script.js
@@ -0,0 +1,15 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media stream, pass that to video element, then play.
+async function selectMediaStream() {
+    try{
+
+    } catch(error) {
+        // Catch errors
+        console.log('Whoops, something went wrong:' error);
+    }
+}
+
+// On load
+selectMediaStream();
